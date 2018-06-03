d22594f80ee03a1b9e67551e74a05ae6c3f7562f
anxgang <anxgang@gmail.com>
2018-06-03 16:50:45 +0800

    add true or false

--- decode.php
+++ decode.php
@@ -5,10 +5,15 @@ use Zxing\QrReader;
 
 $url = $_GET["u"];
 if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$url)) {
-  echo "Invalid URL"; 
+  echo "false, Invalid URL";
 } else {
   $img = 'tmpfile.png';
   file_put_contents($img, file_get_contents($url));
   $qrcode = new QrReader($img);
-  echo $text = $qrcode->text(); //return decoded text from QR Code
+  $text = $qrcode->text(); //return decoded text from QR Code
+  if( $text!='' ){
+    echo "true, $text";
+  }else{
+    echo "false, Failed to decode";
+  }
 }
